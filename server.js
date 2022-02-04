641960bbfc767c5a482b528d4e3820e7f0d91da0
joaomateuus <joaomateusmoraisdeamaral@gmail.com>
2022-02-03 22:56:48 -0300

    server running

--- server.js
+++ server.js
@@ -8,6 +8,10 @@
 const app = require('./src/app');
 const port = process.env.PORT || 3000;
 
+app.get('/',(req, res)=>{
+    res.json({'message': 'servidor está rodando :P'});
+})
+
 app.listen(port, ()=>{
     console.log('A aplicação está rodando na porta:', port);
 
--- src/app.js
+++ src/app.js
@@ -8,4 +8,8 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.urlencoded( { extended:true } ) );
+app.use(express.json());
+
+
 module.exports = app;
